1628ff8c52e9d5c52fb6fe7364b3b0fc60b7f626
ahmadzaid-sattar <ahmadzaid.sattar@gmail.com>
2018-07-10 15:58:53 +0500

    generated files on 2018-07-10 15:58:53

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include"Point.h"
 #include"Unit.h"
+#include"Self.h"
 
 using namespace std;
 
 int main()
 {
-
+    Self s;
+    cout<<s.getWeapon()<<endl;
+    s.setWeapon(1);
+    cout<<s.getWeapon();
     return 0;
 }
